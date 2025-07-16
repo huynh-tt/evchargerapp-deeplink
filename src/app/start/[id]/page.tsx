84365240406239aada8d28d2c66fc22283198a7f
Zay <huynhtt.dev@gmail.com>
2025-07-16 15:38:24 +0700

    feat: fix by myself

--- src/app/start/[id]/page.tsx
+++ src/app/start/[id]/page.tsx
@@ -1,19 +1,8 @@
-// src/app/start/[id]/page.tsx
-
-interface PageProps {
-  params: {
-    id: string; // 'id' matches the folder name '[id]'
-  };
-  searchParams?: { [key: string]: string | string[] | undefined }; // Optional, if you use search params
-}
-
-export default function StartPage({ params }: PageProps) {
-  const { id } = params;
-
-  return (
-    <div>
-      <h1>Start Page for ID: {id}</h1>
-      {/* Your component content */}
-    </div>
-  );
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  return <div>Deep link ID: {id}</div>;
 }
